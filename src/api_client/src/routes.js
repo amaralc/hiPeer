ed48c1941884aa9d289465900a96e9eb51d02e60
amaralc <amaral.calil@gmail.com>
2020-01-22 00:35:46 -0300

    API-CLIENT / FEATURE / Cria rota de teste para criar e retornar usuario

--- src/api_client/src/routes.js
+++ src/api_client/src/routes.js
@@ -1,12 +1,19 @@
 // IMPORTS  --------------------------------------------------------------------
 import { Router } from 'express';
+import User from './app/models/User';
 
 // CONTENT ---------------------------------------------------------------------
 const routes = new Router();
 
 // define rota raiz
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'name two',
+    email: 'email2@gmail.com',
+    password_hash: 'mypasswordhash',
+  });
+
+  return res.json(user);
 });
 
 // EXPORTS  ---------------------------------------------------------------------
